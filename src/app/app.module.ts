e21e53c43a78d28a2122faccc7ae22133e6dce75
HuuThuan19406 <thuannh19406@st.uel.edu.vn>
2021-10-24 16:35:41 +0700

    demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TieuDeComponent } from './tieu-de/tieu-de.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TieuDeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      { path: 'tieu-de', component: TieuDeComponent }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/tieu-de/tieu-de.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tieu-de',
+  templateUrl: './tieu-de.component.html',
+  styleUrls: ['./tieu-de.component.scss']
+})
+export class TieuDeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
