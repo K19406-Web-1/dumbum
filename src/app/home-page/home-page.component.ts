5b80251ffa741546ed6dc03f97899e1c2f14150c
HuuThuan19406 <thuannh19406@st.uel.edu.vn>
2021-11-06 12:24:38 +0700

    fix css

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -11,16 +11,16 @@ export class HomePageComponent implements OnInit {
 
   products: Menu[] = [
     new Menu([
-      new MenuItem(new Product('Gà sấy khô', 40000, '1-min.png'), 'product-detail'),
-      new MenuItem(new Product('Gà sấy khô', 40000, '2-min.png')),
-      new MenuItem(new Product('Gà sấy khô', 40000, '3-min.png')),
-      new MenuItem(new Product('Gà sấy khô', 40000, '4-min.png'))
+      new MenuItem(new Product('Mít sấy', 40000, '5-min.png')),
+      new MenuItem(new Product('Khô cá Thiêu que', 40000, '6-min.png')),
+      new MenuItem(new Product('Rong biển', 40000, '7-min.png')),
+      new MenuItem(new Product('Da heo', 40000, '8-min.png'))
     ], 'SẢN PHẨM NỔI BẬT'),
     new Menu([
-      new MenuItem(new Product('Gà sấy khô', 40000, '5-min.png')),
-      new MenuItem(new Product('Gà sấy khô', 40000, '6-min.png')),
-      new MenuItem(new Product('Gà sấy khô', 40000, '7-min.png')),
-      new MenuItem(new Product('Gà sấy khô', 40000, '8-min.png'))
+      new MenuItem(new Product('Gà sấy khô', 40000, '1-min.png'), 'product-detail'),
+      new MenuItem(new Product('Cơm cháy', 40000, '2-min.png')),
+      new MenuItem(new Product('Cơm cháy', 40000, '3-min.png')),
+      new MenuItem(new Product('Mực xé', 40000, '4-min.png'))
     ], 'GIẢM GIÁ')
   ]
 
