0848d3fe992a3fd2673cbe770659ac48fa8f2376
hoangvn19406 <60832910+hoangvn19406@users.noreply.github.com>
2021-12-02 21:29:32 +0700

    Add files via upload
    chính sách đổi trả

---
+++ src/app/chinhsach.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chinhsach',
+  templateUrl: './chinhsach.component.html',
+  styleUrls: ['./chinhsach.component.scss']
+})
+export class ChinhsachComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
