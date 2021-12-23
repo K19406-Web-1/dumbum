1db70bd1ffc1e90e5eada883034187cf994ae917
HuuThuan19406 <thuannh19406@st.uel.edu.vn>
2021-12-23 21:42:59 +0700

    Thêm phương thức lấy Product theo Name

--- src/models/dbcontext.ts
+++ src/models/dbcontext.ts
@@ -7,3 +7,30 @@ export async function loadProducts(category: categories, type: types = types["Tá
   return await fetch(`assets/data/${categories[category]}.json`)
     .then(response => response.json().then(data => data as Product[]));
 }
+
+/**
+ * @param productName Name of the product
+ * @return Product if found, otherwise null
+ */
+export async function loadProduct(productName: string): Promise<Product | null> {
+  let product: Product | null = null;
+  for (const category in categories) {
+    if (!isNaN(Number(category))) {
+      product = await loadProducts(category as unknown as categories)
+        .then(products => {
+          let result: Product | null = null;
+          products
+            .forEach(item => {
+              if (item.name === productName) {
+                result = item;
+              }
+            });
+          return result;
+        });
+      if (product) {
+        return product;
+      }
+    }
+  }
+  return product;
+}
