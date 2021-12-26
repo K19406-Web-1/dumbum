2a8b7192a4a87224d0b55cd8ecbf486529c26fd9
HuuThuan19406 <thuannh19406@st.uel.edu.vn>
2021-12-26 17:39:55 +0700

    Co-authored-by: khngann <khngann@users.noreply.github.com> Co-authored-by: thuongqiong <thuongqiong@users.noreply.github.com> Co-authored-by: truongbaotran <truongbaotran@users.noreply.github.com> Co-authored-by: hoangvn19406 <hoangvn19406@users.noreply.github.com>

--- src/app/product-template/product-template.component.ts
+++ src/app/product-template/product-template.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Cart, CartItems } from 'src/models/cart';
 import { ProductBase } from 'src/models/product';
 
 @Component({
@@ -9,11 +10,26 @@ import { ProductBase } from 'src/models/product';
 export class ProductTemplateComponent implements OnInit {
 
   @Input() product!: ProductBase;
-  @Output() seeMore = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  seeMore() {
+    location.href = '/product-detail?name=' + this.product.name;
+  }
 
+  addToCart() {
+    let cartLS = localStorage.getItem('cart');
+    let cart: Cart;
+    if (cartLS != null) {
+      cart =  Cart.parse(cartLS);
+    }
+    else {
+      cart = new Cart();
+    }
+    cart.addUnique(new CartItems(this.product.name));
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }
 }
